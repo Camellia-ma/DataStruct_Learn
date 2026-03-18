afe879ed944d916cc083967b6cce729f1e8a91b3
camellia_liu <camellia_liu@example.com>
2026-03-18 21:09:47 +0800

    编写Test.c文件来测试架构是否可以正常运行

--- src/Test.c
+++ src/Test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+
+int get_max_number(const int nums[],const int size) {
+    int max_num = 0;
+    for (int i = 0;i < size;i++) {
+        max_num =  nums[i] > max_num ? nums[i] : max_num;
+    }
+    return max_num;
+}
+
 int main() {
-    printf("This is a Test Function");
+    const int nums[9] = {1,2,3,4,5,6,7,8,100};
+    printf("max number is: %d\n",get_max_number(nums,sizeof(nums) / sizeof(nums[1])));
     return 0;
 }
 
